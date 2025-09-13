64bdd7a5e3585646a9bf70f7e71cc840d4c0c53c
vpriyadharshini29 <vpriyadharshini29@gmail.com>
2025-09-13 14:20:36 +0530

    online book store

--- store/front_urls.py
+++
@@ -1,4 +0,0 @@
-from django.urls import path
-from django.shortcuts import render
-def books_view(request): return render(request, 'store/books.html')
-urlpatterns = [path('', books_view, name='books')]
